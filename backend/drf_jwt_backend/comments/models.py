82cfd73c9aab065c1dce0ab40e1daa391866f9d9
h-latta <h.latta@yahoo.com>
2022-03-16 13:52:27 -0500

    Created Separate App for Comments and Replies

---
+++ backend/drf_jwt_backend/comments/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model 
+
+User = get_user_model()
+
+# Create your models here.
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=500)
+    text = models.CharField(max_length=250)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+
+
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey
+    text = models.CharField(max_length=255)
