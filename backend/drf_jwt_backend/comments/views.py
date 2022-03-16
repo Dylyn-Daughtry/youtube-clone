8678014b6dbfda6e09ebc604be89b042e19df022
h-latta <h.latta@yahoo.com>
2022-03-16 14:03:17 -0500

    Started Views File, Updated Project URL File to Start Path for Comments URL File

--- backend/drf_jwt_backend/comments/views.py
+++ backend/drf_jwt_backend/comments/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Comments
+from .models import Reply
+from .serializers import CommentSerializer
+from . serializers import ReplySerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
