6b4a50f38dd29c9cbe4feb142620865c2e71717f
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-03-17 14:23:16 -0400

    Put Request Created in views.py

--- backend/drf_jwt_backend/comments/views.py
+++ backend/drf_jwt_backend/comments/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Comments
 from .models import Reply
@@ -6,6 +6,7 @@ from .serializers import CommentSerializer
 from . serializers import ReplySerializer
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import OR, IsAuthenticated, AllowAny
 
 # Create your views here.
 
@@ -26,3 +27,14 @@ def create_comment(request):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['Put'])
+@permission_classes([IsAuthenticated])
+def update_comment_by_id(request, id):
+    comment = Comments.objects.get(id = id)
+    serializer = CommentSerializer(comment, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
