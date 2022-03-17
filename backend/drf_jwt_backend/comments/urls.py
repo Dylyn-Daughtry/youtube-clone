7c6dc8d161abf9acfd7721fb4c1961a38f07c4fa
h-latta <h.latta@yahoo.com>
2022-03-17 13:26:10 -0500

    Created POST Request in Views File

--- backend/drf_jwt_backend/comments/urls.py
+++ backend/drf_jwt_backend/comments/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('video_id', views.get_comment),
-    path('', views.create_comment)
+    path('', views.get_create_comment),
 ]
--- backend/drf_jwt_backend/comments/views.py
+++ backend/drf_jwt_backend/comments/views.py
@@ -11,24 +11,29 @@ from rest_framework.permissions import OR, IsAuthenticated, AllowAny
 # Create your views here.
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def get_comment(request):
     comment_sort = request.query_params.get('video_id')
     comment = Comments.objects.all()
 
     if comment_sort:
         comment = comment.filter(video_id=comment_sort)
+        
 
     serializer = CommentSerializer(comment, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
-def create_comment(request):
-    serializer = CommentSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+@permission_classes([IsAuthenticated])
+def get_create_comment(request):
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['Put'])
+@api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def update_comment_by_id(request, id):
     comment = Comments.objects.get(id = id)
