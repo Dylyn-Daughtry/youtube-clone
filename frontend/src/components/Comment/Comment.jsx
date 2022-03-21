7c8b72ac3777bc956aae9ce90b12e2698aadfd73
h-latta <h.latta@yahoo.com>
2022-03-21 13:54:03 -0500

    Started Comment Component, Modified File Extension for HomePage

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -3,12 +3,40 @@ import axios from 'axios';
 
 const Comment = (props) => {
 
+    
     const [comment, SetComment] = useState([])
+    
+    let defaultComment = {
+        user: '',
+        text: '',
+        likes: '',
+        dislikes: ''
+    }
+    async function returnComment(){
+        retrieveComment = await axios.get(`http://127.0.0.1:8000/api/comments/video_id?${props.videoId}`)
 
+    }
     return ( 
         <div>
-            <h2>Username</h2>
-            <span>Comment</span>
+            <h2>Comments</h2>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {retrieveComment.map(comment => {
+                    return (
+                        <tr key = {comment.id}>
+                            <td>{comment.text}</td>
+                            <td>{comment.likes}</td>
+                            <td>{comment.dislikes}</td>                          
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
         </div>
      );
 }
