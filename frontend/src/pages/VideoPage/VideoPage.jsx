1c7cbf008beb80e34bd733fb6d7d563f5069f645
h-latta <h.latta@yahoo.com>
2022-03-21 15:16:39 -0500

    Eliminated Properties of Car Files, Troubleshooting Search Bar Functionality

--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,6 +1,4 @@
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
-import React, { useState } from 'react';
-import axios from "axios";
 import Comment from "../../components/Comment/Comment";
 
 const VideoPage = (props) => {
