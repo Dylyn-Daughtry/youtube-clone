421970351dc1b21f5b3b9489e03dcf81f98553f0
h-latta <h.latta@yahoo.com>
2022-03-21 15:40:43 -0500

    Troubleshooting Comment Display

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -13,28 +13,23 @@ const Comment = (props) => {
 
     return ( 
         <div>
-            {returnComment()}
             <h2>Comments</h2>
             <table className='table'>
                 <thead>
                     <tr>
-                        <th>{comment.user_id.username}</th>
+                        <th>{comment.user_id}</th>
                     </tr>
                 </thead>
                 <tbody>
-                {comment.map(comment => {
-                    return (
-                        <tr key = {comment.id}>
-                            <td>{comment.text}</td>
-                            <td>{comment.likes}</td>
-                            <td>{comment.dislikes}</td>                          
-                        </tr>
-                    )
-                })}
+                    <tr key = {comment.id}>
+                        <td>{comment.text}</td>
+                        <td>{comment.likes}</td>
+                        <td>{comment.dislikes}</td>                          
+                    </tr>
                 </tbody>
             </table>
         </div>
-     );
+    )
 }
  
 export default Comment;
--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 const VideoPlayer = () => {
     return ( 
         <iframe id="ytplayer" type='text/html' width='640' height='360'
-         src="https://www.googleapis.com/youtube/v3/search?q=bob-ross&key=AIzaSyBDtWaBV1dE1YDItvhkwBBibwZnWB9ethc" frameborder='0'>
+         src="https://www.youtube.com/embed/jNQXAC9IVRw" frameborder='0'>
         </iframe>
      );
 }
